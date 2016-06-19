5b914016f9934095f4b3571a631c03c5e4ce33e8
tabrezakhtar <t_akhtar232@MisterT-Pro.lan>
2016-06-19 13:52:50 +0100

    doing react tutorial

---
+++ public/scripts/tut.js
@@ -0,0 +1,94 @@
+/*
+- CommentBox
+  - CommentList
+    - Comment
+  - CommentForm
+*/
+// tutorial1.js
+var data = [
+  {id: 1, author: "Pete Hunt", text: "This is one comment"},
+  {id: 2, author: "Jordan Walke", text: "This is *another* comment"},
+  {id: 3, author: "dude", text: "hello"}
+];
+var CommentBox = React.createClass({
+    // tutorial8.js
+
+    render: function() {
+        return (
+              <div className="commentBox">
+                    <h1>Comments</h1>
+                    <CommentList data={this.props.data} />
+                    <CommentForm />
+              </div>
+        );
+    }
+});
+
+
+
+
+// tutorial2.js
+var CommentList = React.createClass({
+
+  render: function() {
+      var commentNodes = this.props.data.map(function(comment) {
+            return (
+              <Comment author={comment.author} key={comment.id}>
+                {comment.text}
+              </Comment>
+            );
+          });
+
+
+
+
+
+
+    return (
+      <div className="commentList">
+        {commentNodes}
+      </div>
+    );
+  }
+});
+
+
+
+
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+
+// tutorial4.js
+var Comment = React.createClass({
+    rawMarkup: function() {
+        var md = new Remarkable();
+        var rawMarkup = md.render(this.props.children.toString());
+        return { __html: rawMarkup };
+    },
+
+    render: function() {
+        var md = new Remarkable();
+
+        return (
+            <div className="comment">
+                <h2 className="commentAuthor">
+                    {this.props.author}
+                </h2>
+                <span dangerouslySetInnerHTML={this.rawMarkup()} />
+            </div>
+        );
+    }
+});
+
+
+ReactDOM.render(
+    <CommentBox data={data} />, document.getElementById('content')
+);
